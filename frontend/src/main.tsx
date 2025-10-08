cf598f7232f8bcf8264b6d1b377fb700ebbb7633
sief aqel <sief.aqel@teklabz.com>
2025-10-08 10:48:59 +0300

    add /api to base url

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV === "production") {
   disableReactDevTools();
 }
 
-const baseUrl = "https://workouts-api-t67z.onrender.com";
+const baseUrl = "https://workouts-api-t67z.onrender.com/api/";
 axios.defaults.baseURL = baseUrl;
 axios.defaults.headers.common["Content-Type"] = "application/json";
 axios.defaults.headers.common["Accept"] = "application/json";
